d1cc9597563b5a336f9d83e6d2be8703b0083162
Long Tran <kevintran1402@gmail.com>
2021-11-15 15:10:53 +0700

    fix props binding

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ import GamePlay from '../components/GamePlay';
 import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout';
+import Welcome from '../components/Welcome';
 
-export default function Home() {
+export default function Home({ welcomeData, users }) {
   return (
     <>
       <Head>
@@ -14,9 +15,42 @@ export default function Home() {
         <meta name='keywords' content='metadino' />
       </Head>
       <Layout>
+        <Welcome name={welcomeData.name} age={welcomeData.age} />
         <Header />
-        <GamePlay />
+        <GamePlay users={users} />
       </Layout>
     </>
   );
+}
+
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const welcomeData = getWelcome()
+  const users = await getUsers()
+  return {
+    props: {
+      welcomeData,
+      users
+    },
+  }
+}
+
+function getWelcome() {
+  return {
+    name: 'Long',
+    age: 33
+  }
+}
+
+async function getUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
+
+  if (!users) {
+    return {
+      notFound: true,
+    };
+  }
+  return users
 }

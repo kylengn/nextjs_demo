1a5d2496beaa55a36576e149d23d83fa7f7d2853
Kyle Nguyen <kyle.nguyen304@gmail.com>
2021-11-24 01:50:14 -0500

    updated Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -46,9 +46,9 @@ const Navbar = () => {
               xmlns='http://www.w3.org/2000/svg'
             >
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
                 d='M4 6h16M4 12h16M4 18h16'
               ></path>
             </svg>

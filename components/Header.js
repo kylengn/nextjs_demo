16cd017adc6b3bac0225d6e39ee1b04cec9e9e99
Kyle Nguyen <kyle.nguyen304@gmail.com>
2021-11-28 08:44:37 -0500

    updated header layout and navbar

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from '../styles/Header.module.css';
 
 const Header = () => {
-  return <div className={styles.banner}></div>;
+  return (
+    <div className={styles.banner}>
+      <h1 className='text-5xl'>Header</h1>
+    </div>
+  );
 };
 
 export default Header;
